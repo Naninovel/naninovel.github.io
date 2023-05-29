539d49052d2fea79ff1801ef52a16f3e0d95fe7e
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2023-05-30 02:44:15 +0300

    remove unused

--- docs/.vitepress/ext/markdown.ts
+++ docs/.vitepress/ext/markdown.ts
@@ -3,8 +3,8 @@ import regexp from "markdown-it-regexp";
 
 export function configureMarkdown(md: MarkdownRenderer) {
     md.use(regexp(/\[@(\w+?)]/, m => buildCommandTags(md, m)));
-    md.use(regexp(/\[!(\w+?)]/, m => buildVideoTags(md, m)));
-    md.use(regexp(/\[!!(.+?)]/, m => buildYouTubeTags(md, m)));
+    md.use(regexp(/\[!(\w+?)]/, buildVideoTags));
+    md.use(regexp(/\[!!(.+?)]/, buildYouTubeTags));
 }
 
 function buildCommandTags(md: MarkdownRenderer, match: string) {
@@ -15,15 +15,15 @@ function buildCommandTags(md: MarkdownRenderer, match: string) {
         else if (route.startsWith("/zh/")) url = "/zh" + url;
         else if (route.startsWith("/ru/")) url = "/ru" + url;
     }
-    return `<a href="${url}" class="" target="_blank"><code>@${match[1]}</code></a>`;
+    return `<a href="${url}" target="_blank"><code>@${match[1]}</code></a>`;
 }
 
-function buildVideoTags(md: MarkdownRenderer, match: string) {
+function buildVideoTags(match: string) {
     const source = `<source src="https://i.gyazo.com/${match[1]}.mp4" type="video/mp4">`;
     return `<video class="video" loop autoplay muted>${source}</video>`;
 }
 
-function buildYouTubeTags(md: MarkdownRenderer, match: string) {
+function buildYouTubeTags(match: string) {
     const iframe = `<iframe src="https://www.youtube-nocookie.com/embed/${match[1]}" allowfullscreen></iframe>`;
     return `<div class="video-container">${iframe}</div>`;
 }
