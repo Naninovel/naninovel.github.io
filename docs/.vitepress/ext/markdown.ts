7020a65cc4fc4b2d905560e4de8c4ef85b51cd3b
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2023-05-29 18:05:11 +0300

    iteration

--- docs/.vitepress/ext/markdown.ts
+++ docs/.vitepress/ext/markdown.ts
@@ -2,20 +2,28 @@
 import regexp from "markdown-it-regexp";
 
 export function configureMarkdown(md: MarkdownRenderer) {
-    md.use(regexp(/\[@(\w+?)]/, function (match, _) {
-        let url = `/api/#${match[1].toLowerCase()}`;
-        if (md["links"] !== undefined) {
-            let route = md["links"][0];
-            if (route.startsWith("/ja/")) url = "/ja" + url;
-            else if (route.startsWith("/zh/")) url = "/zh" + url;
-            else if (route.startsWith("/ru/")) url = "/ru" + url;
-        }
-        return `<a href="${url}" class="" target="_blank"><code>@${match[1]}</code></a>`;
-    }));
-    md.use(regexp(/\[!(\w+?)]/, function (match, _) {
-        return `<video class="video" loop autoplay muted><source src="https://i.gyazo.com/${match[1]}.mp4" type="video/mp4"></video>`;
-    }));
-    md.use(regexp(/\[!!(.+?)]/, function (match, _) {
-        return `<div class="video-container"><iframe src="https://www.youtube-nocookie.com/embed/${match[1]}" allowfullscreen></iframe></div>`;
-    }));
+    md.use(regexp(/\[@(\w+?)]/, m => buildCommandTags(md, m)));
+    md.use(regexp(/\[!(\w+?)]/, m => buildVideoTags(md, m)));
+    md.use(regexp(/\[!!(.+?)]/, m => buildYouTubeTags(md, m)));
+}
+
+function buildCommandTags(md: MarkdownRenderer, match: string) {
+    let url = `/api/#${match[1].toLowerCase()}`;
+    if (md["links"] !== undefined) {
+        let route = md["links"][0];
+        if (route.startsWith("/ja/")) url = "/ja" + url;
+        else if (route.startsWith("/zh/")) url = "/zh" + url;
+        else if (route.startsWith("/ru/")) url = "/ru" + url;
+    }
+    return `<a href="${url}" class="" target="_blank"><code>@${match[1]}</code></a>`;
+}
+
+function buildVideoTags(md: MarkdownRenderer, match: string) {
+    const source = `<source src="https://i.gyazo.com/${match[1]}.mp4" type="video/mp4"/>`;
+    return `<video class="video" loop autoplay muted>${source}</video>`;
+}
+
+function buildYouTubeTags(md: MarkdownRenderer, match: string) {
+    const iframe = `<iframe src="https://www.youtube-nocookie.com/embed/${match[1]}" allowfullscreen/>`;
+    return `<div class="video-container">${iframe}</div>`;
 }
